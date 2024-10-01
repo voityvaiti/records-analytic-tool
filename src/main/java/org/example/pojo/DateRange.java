771fa82dc287b52b57233417b5bf9a3d802d2c03
voityvaiti <flashlight@i.ua>
2024-10-01 20:55:21 +0300

    Pojo for date range

---
+++ src/main/java/org/example/pojo/DateRange.java
@@ -0,0 +1,22 @@
+package org.example.pojo;
+
+import java.time.LocalDate;
+
+public class DateRange {
+    private final LocalDate dateFrom;
+    private final LocalDate dateTo;
+
+    public DateRange(LocalDate dateFrom, LocalDate dateTo) {
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+    }
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+}
+
