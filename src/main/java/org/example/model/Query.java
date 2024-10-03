11f35a23e629f5639224c1a2d285c35dbc3c5fae
voityvaiti <flashlight@i.ua>
2024-10-03 20:57:49 +0300

    Model classes of query and record

---
+++ src/main/java/org/example/model/Query.java
@@ -0,0 +1,66 @@
+package org.example.model;
+
+import org.example.domain.Question;
+import org.example.domain.ResponseType;
+import org.example.domain.Service;
+import org.example.pojo.DateRange;
+import org.example.util.ParsingUtils;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class Query {
+
+    private final Service service;
+    private final Question question;
+    private final ResponseType responseType;
+    private final LocalDate dateFrom;
+    private final LocalDate dateTo;
+
+    public Query(Service service, Question question, ResponseType responseType, LocalDate dateFrom, LocalDate dateTo) {
+        this.service = service;
+        this.question = question;
+        this.responseType = responseType;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+    }
+
+    public static Optional<Query> parseQuery(String line) {
+        try {
+            String[] parts = line.split(" ");
+            if (parts.length != 5 || parts[0].charAt(0) != 'D') {
+                throw new IllegalArgumentException("Invalid query format.");
+            }
+
+            Service service;
+            if(parts[1].equals("*")) {
+                service = new Service(null, null);
+            } else {
+                service = ParsingUtils.parseService(parts[1]);
+            }
+
+            Question question;
+            if(parts[2].equals("*")) {
+                question = new Question(null, null, null);
+            } else {
+                question = ParsingUtils.parseQuestion(parts[2]);
+            }
+
+            ResponseType responseType = ResponseType.valueOf(parts[3]);
+            DateRange dateRange = ParsingUtils.parseDateRange(parts[4]);
+
+            return Optional.of(new Query(service, question, responseType, dateRange.getDateFrom(), dateRange.getDateTo()));
+
+        } catch (Exception e) {
+            System.err.println("Line parsing error: " + line + " - " + e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public Service getService() { return service; }
+    public Question getQuestion() { return question; }
+    public ResponseType getResponseType() { return responseType; }
+    public LocalDate getDateFrom() { return dateFrom; }
+    public LocalDate getDateTo() { return dateTo; }
+}
+
---
+++ src/main/java/org/example/model/Record.java
@@ -0,0 +1,55 @@
+package org.example.model;
+
+import org.example.domain.Question;
+import org.example.domain.ResponseType;
+import org.example.domain.Service;
+import org.example.util.ParsingUtils;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class Record {
+
+    private final Service service;
+    private final Question question;
+    private final ResponseType responseType;
+    private final LocalDate date;
+    private final int waitingTime;
+
+    public Record(Service service, Question question, ResponseType responseType, LocalDate date, int waitingTime) {
+        this.service = service;
+        this.question = question;
+        this.responseType = responseType;
+        this.date = date;
+        this.waitingTime = waitingTime;
+    }
+
+    public static Optional<Record> parseRecord(String line) {
+        try {
+            String[] parts = line.split(" ");
+            if (parts.length != 6 || parts[0].charAt(0) != 'C') {
+                throw new IllegalArgumentException("Invalid record format.");
+            }
+
+            Service service = ParsingUtils.parseService(parts[1]);
+            Question question = ParsingUtils.parseQuestion(parts[2]);
+            ResponseType responseType = ResponseType.valueOf(parts[3]);
+            LocalDate date = ParsingUtils.parseDate(parts[4]);
+            int waitingTime = Integer.parseInt(parts[5]);
+
+            return Optional.of(new Record(service, question, responseType, date, waitingTime));
+
+        } catch (Exception e) {
+            System.err.println("String parsing error: " + line + " - " + e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public Service getService() { return service; }
+    public Question getQuestion() { return question; }
+    public ResponseType getResponseType() { return responseType; }
+    public LocalDate getDate() { return date; }
+    public int getWaitingTime() { return waitingTime; }
+}
+
+
